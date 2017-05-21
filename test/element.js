e773f7304273cb1f53081e4a69c4228c479bd101
ajlopez <webmaster@ajlopez.com>
2017-05-21 14:38:54 -0300

    Attributes get and set named item

--- test/element.js
+++ test/element.js
@@ -33,3 +33,23 @@ exports['no attributes'] = function (test) {
 	test.ok(element.attributes);
 	test.equal(element.attributes.length, 0);
 };
+
+exports['get named attribute as null'] = function (test) {
+	var element = domie.document().createElement('h1');
+	
+	test.equal(element.attributes.getNamedItem('foo'), null);
+};
+
+exports['set and get named attribute'] = function (test) {
+	var document = domie.document();
+	var element = document.createElement('h1');
+	var attr = document.createAttribute('id');
+	attr.value = 42;
+	element.attributes.setNamedItem(attr);
+	
+	var result = element.attributes.getNamedItem('id');
+	
+	test.ok(result);
+	test.equal(result.name, 'id');
+	test.equal(result.value, '42');
+};
